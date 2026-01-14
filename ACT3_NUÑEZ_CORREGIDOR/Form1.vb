ca1c5b1198622e9379566ae812a4f182ea43349d
Abenad1123 <103361048+Abenad1123@users.noreply.github.com>
2026-01-14 13:28:15 +0800

    Add project files.

---
+++ ACT3_NUÑEZ_CORREGIDOR/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    ' ---- STANDARD FEES ----
+    Public Shared tuition_fee As Integer = 140143
+    Public Shared miscellaneous_fee As Integer = 5150
+
+    ' ---- ITEMS ----
+    Public Shared school_bus_fee_yearly As Integer = 80000
+    Public Shared shuttle_fee_yearly As Integer = 50000
+    Public Shared books_fee As Integer
+
+    Public Shared reg_uniform_fullSet() As Integer = {800, 900, 1040, 1160, 1300, 1500}
+    Public Shared pe_uniform_buttom_fullSet() As Integer = {800, 900, 1040, 1160, 1300, 1500}
+
+    ' ---- DISCOUNTS ----
+    ' Para sa mga PWD
+    Public Shared discount_pwd As Double = 0.1
+    ' Kapag may 2 or more na kapamilyang enrolled sa school
+    Public Shared discount_family As Double = 0.03
+    ' Kapag anak ng employee sa school
+    Public Shared discount_employee As Double = 0
+
+    Public Shared intial_pay As Double = 0
+    Public Shared total_pay As Double = 0
+
+    Public Shared reg_uniform_set_amount As Integer
+    Public Shared pe_uniform_set_amount As Integer
+
+    Public Shared reg_uniform_set_size As Integer
+    Public Shared pe_uniform_set_size As Integer
+
+End Class
---
+++ ACT3_NUÑEZ_CORREGIDOR/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ACT3_NUÑEZ_CORREGIDOR")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("ACT3_NUÑEZ_CORREGIDOR")>
+<Assembly: AssemblyCopyright("Copyright © HP 2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2a1db35a-0ec0-4c3b-a1e1-fb9ea2e867a2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
